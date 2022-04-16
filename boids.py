6612ae9c13f9a01d6217a8d3dce1a10b75edad2b
Nitish-Naineni <96668765+Nitish-Naineni@users.noreply.github.com>
2022-04-15 21:38:23 -0400

    changed everything

--- boids.py
+++ boids.py
@@ -3,9 +3,9 @@ from pettingzoo.mpe._mpe_utils.simple_env import SimpleEnv, make_env
 from scenario import Scenario
 
 class raw_env(SimpleEnv):
-    def __init__(self, num_agents,num_adversaries, max_cycles=25, continuous_actions=True):
-        scenario = Scenario(num_agents,num_adversaries)
-        world = scenario.make_world()
+    def __init__(self, num_good=3, num_adversaries=1, num_obstacles=2, max_cycles=25, continuous_actions=False):
+        scenario = Scenario()
+        world = scenario.make_world(num_good, num_adversaries, num_obstacles)
         super().__init__(scenario, world, max_cycles, continuous_actions)
         self.metadata['name'] = "boids"
 
