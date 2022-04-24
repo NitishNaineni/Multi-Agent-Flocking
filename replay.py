e7316c64bc4f70b7bbc2c33355a645d4693c5de3
Nitish-Naineni <96668765+Nitish-Naineni@users.noreply.github.com>
2022-04-23 23:43:33 -0400

    prioritized experience replay added

---
+++ replay.py
@@ -0,0 +1,41 @@
+from importlib.machinery import all_suffixes
+from platform import mac_ver
+import random
+import numpy as np
+from collections import deque
+
+class Prioritized_Experience_Replay:
+    def __init__(self, size, alpha, sample_size):
+        self.size = size
+        self.alpha = alpha
+        self.sample_size = sample_size
+        self.buffer = deque(maxlen=size)
+        self.priorities = deque(maxlen=size)
+        self.offset = 1
+        self.p_total = 0
+        self.timestep = 0
+
+    def push(self,data,loss):
+        p = (abs(loss) + self.offset)**self.alpha
+        
+        if self.timestep >= self.size:
+            last = self.priorities[0]
+            self.p_total -= last
+
+        self.priorities.append(p)
+        self.buffer.append(data)
+
+        self.p_total += p
+        self.timestep += 1
+
+    def sample(self):
+        seed = np.random.randint(0,2**31)
+        sample_prob = np.array(self.priorities)/self.p_total
+        np.random.seed(seed)
+        sampled = np.random.choice(self.buffer,self.sample_size,p=sample_prob)
+        np.random.seed(seed)
+        priorities = np.random.choice(self.priorities,self.sample_size,p=sample_prob)
+        return sampled,priorities
+
+    def is_full(self):
+        return self.timestep >= self.size
