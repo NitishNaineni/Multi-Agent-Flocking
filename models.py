fb5bc916944def4542b003c856ba7cc12298dc29
Nitish-Naineni <96668765+Nitish-Naineni@users.noreply.github.com>
2022-05-05 13:35:24 -0400

    added config and ppo

--- models.py
+++ models.py
@@ -4,6 +4,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import math
+import random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+
 class Actor():
          
     def __init__(self, n_obs, n_actions, n_hNodes = 64):
@@ -51,6 +63,37 @@ class Critic():
 
         return x
 
+class ActorCriticPPO(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        super(ActorCriticPPO, self).__init__()
+        
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+        
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+        )
+        self.log_std = nn.Parameter(torch.ones(1, num_outputs) * std)
+        
+        self.apply(init_weights)
+        
+    def forward(self, x):
+        value = self.critic(x)
+        mu    = self.actor(x)
+        std   = self.log_std.exp().expand_as(mu)
+        dist  = Normal(mu, std)
+        return dist, value
+
+
+def init_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, mean=0., std=0.1)
+            nn.init.constant_(m.bias, 0.1)
 
 
 
